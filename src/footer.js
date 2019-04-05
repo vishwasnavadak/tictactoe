944b74a0670c6859edf12e3df8afe160fc89dbc2
Vishwas Navada <navadavishwas@hotmail.com>
2019-04-06 00:52:28 +0530

    remove reset

--- src/footer.js
+++ src/footer.js
@@ -4,9 +4,6 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
   return (
     <div className="footer">
-      <Link to="#" onClick="window.location.reload()">
-        Reset
-      </Link>
       <Link to="/">Go Back</Link>
     </div>
   );
