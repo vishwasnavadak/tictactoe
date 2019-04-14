e95c301f04a36d21aa1335b05e9000ad95dff4a7
Vishwas <hi@vishwas.tech>
2019-04-14 12:57:19 +0530

    fixing routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { hydrate } from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./index.css";
 
@@ -9,14 +9,14 @@ import Game2 from "./Game2";
 import Game3 from "./Game3";
 import Game4 from "./Game4";
 
-ReactDOM.render(
+hydrate(
   <Router>
-    <div>
+    <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/2x2" component={Game2} />
       <Route path="/3x3" component={Game3} />
       <Route path="/4x4" component={Game4} />
-    </div>
+    </Switch>
   </Router>,
   document.getElementById("root")
 );
