e0285a1d8913e74570d83658d05d6eb4c8ea934a
Vishwas <hi@vishwas.tech>
2019-04-21 10:55:58 +0530

    Updates

---
+++ precache-manifest.162617bcd63d283c78793686e88f58ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ca7f52bc2976bd02b2f237b6803f26",
+    "url": "/tictactoe/static/media/4.23ca7f52.png"
+  },
+  {
+    "revision": "4e1078b9558515cfcebeb7dfcb04cf4f",
+    "url": "/tictactoe/static/media/3.4e1078b9.png"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  },
+  {
+    "revision": "e09f9ab60bbc22ba5657",
+    "url": "/tictactoe/static/js/main.1aaacb1e.chunk.js"
+  },
+  {
+    "revision": "98a021059bac231f2836",
+    "url": "/tictactoe/static/js/2.8a086d96.chunk.js"
+  },
+  {
+    "revision": "e09f9ab60bbc22ba5657",
+    "url": "/tictactoe/static/css/main.fc20eb98.chunk.css"
+  },
+  {
+    "revision": "33acf0cc10fb04493198d358718d68fa",
+    "url": "/tictactoe/index.html"
+  }
+];
