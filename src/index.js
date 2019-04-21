455e8ad817a55cefae3dbd76943e95c48083828b
Vishwas <hi@vishwas.tech>
2019-04-21 11:02:26 +0530

    changing router and deployment homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { hydrate } from "react-dom";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./index.css";
 
@@ -10,13 +10,13 @@ import Game3 from "./Game3";
 import Game4 from "./Game4";
 
 hydrate(
-  <Router>
+  <BrowserRouter>
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/2x2" component={Game2} />
       <Route path="/3x3" component={Game3} />
       <Route path="/4x4" component={Game4} />
     </Switch>
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
